ead5a88c72779bb3ad81b0fc364b1a2850ae687c
Justin Barclay <justincbarclay@gmail.com>
2017-05-03 02:12:06 -0600

    Copy code from Apple for how to interact with KeyChain, look for best means to integrate with current app

--- PartyClient/LoginView.swift
+++ PartyClient/LoginView.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import Security
 
 class LoginController: UIViewController {
 
@@ -17,7 +18,9 @@ class LoginController: UIViewController {
     @IBOutlet weak var _password: UITextField!
     @IBOutlet weak var _loginButton: UIButton!
     
-    let _server = "http://localhost:61222"
+    let _testServer = "http://localhost:61222"
+//    let _server = "http://partyserver.dev/user_token"
+    let _server = "http://localhost:3000/user_token"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +34,7 @@ class LoginController: UIViewController {
 
     @IBAction func loginUser(_ sender: UIButton) {
         let url = URL(string: _server)
+//        let url = URL(string: _testServer)
         let username = _email.text
         let password = _password.text
         
@@ -44,27 +48,42 @@ class LoginController: UIViewController {
         }
         
         // I am lazy and this is a simple easy way to understand how I am building up the JSON string
-        loginInfo["username"] = self._email.text
-        loginInfo["password"] = self._password.text
+        loginInfo["email"] = username
+        loginInfo["password"] = password
         container["auth"] = loginInfo
         
         // Generic setup for making a URL request
         var urlRequest = URLRequest(url: url!)
         urlRequest.httpMethod = "POST"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
         let session = URLSession.shared
     
+        
         do{
-        urlRequest.httpBody = try JSONSerialization.data(withJSONObject: container, options: [])
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: container, options: [])
         } catch {
             print("JSON Error")
             return
         }
+        
         let task = session.dataTask(with: urlRequest) { (data, response, error) in
-            print(data)
+            print(String(data: data!, encoding: String.Encoding.utf8))
             print(response)
             print(error)
         }
         task.resume()
     }
+    
+//    private class func save(service: NSString, data: NSString) {
+//        let dataFromString: NSData = data.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+//        
+//        // Instantiate a new default keychain query
+//        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, service, userAccount, dataFromString], forKeys: [kSecClassValue, kSecAttrServiceValue, kSecAttrAccountValue, kSecValueDataValue])
+//        
+//        // Delete any existing items
+//        SecItemDelete(keychainQuery as CFDictionaryRef)
+//        
+//        // Add the new keychain item
+//        SecItemAdd(keychainQuery as CFDictionaryRef, nil)
+//    }
 }
-
